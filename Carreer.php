fd5c5fa0392314f1aacfa650683d12c91279b4cc
Kinmaro212 <toti98741@gmail.com>
2024-11-03 16:16:18 +0100

    Correction

--- Carreer.php
+++ Carreer.php
@@ -8,4 +8,4 @@ class carreer{
        
         /* ? devant le type int dans ?int $annee_fin signifie que cette propriété  peut être de type int ou null. 
         Cela permet de représenter un cas où l'année de fin n'est pas encore définie ou n'est pas applicable
-        (par exemple, si le joueur est toujours actif dans le club).
+        // (par exemple, si le joueur est toujours actif dans le club).*§private array $carriere = [];*/
